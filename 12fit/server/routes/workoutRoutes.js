a07cccfc42120ba7d02050717bc17a6e4aea8805
‪Mohammad Jamous‬‏ <118446184+MohammadJamous@users.noreply.github.com>
2026-04-09 17:51:45 +0300

    Update workoutRoutes.js

--- 12fit/server/routes/workoutRoutes.js
+++ 12fit/server/routes/workoutRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authMiddleware");
 const {
   getWorkouts,
   createWorkout,
 } = require("../controllers/workoutController");
 
-router.get("/", authMiddleware, getWorkouts);
-router.post("/", authMiddleware, createWorkout);
+router.get("/",  getWorkouts);
+router.post("/", createWorkout);
 
 module.exports = router;

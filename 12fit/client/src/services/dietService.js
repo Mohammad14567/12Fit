f863b8a303a2a1816f40f9d4997283852858d1d1
‪Mohammad Jamous‬‏ <118446184+MohammadJamous@users.noreply.github.com>
2026-04-09 15:54:56 +0300

    Update dietService.js

--- 12fit/client/src/services/dietService.js
+++ 12fit/client/src/services/dietService.js
@@ -2,8 +2,6 @@ import api from "../utils/api";
 
 export const generateDiet = (data, token) => {
   return api.post("/diet", data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  
   });
 };

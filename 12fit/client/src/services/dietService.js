5c2af2a3e2a1c412ca9a8209e17cea70171303c6
‪Mohammad Jamous‬‏ <118446184+MohammadJamous@users.noreply.github.com>
2026-04-18 14:47:23 +0300

    Delete 12fit/client directory

--- 12fit/client/src/services/dietService.js
+++
@@ -1,7 +0,0 @@
-import api from "../utils/api";
-
-export const generateDiet = (data, token) => {
-  return api.post("/diet", data, {
-   
-  });
-};
--- 12fit/client/src/services/workoutService.js
+++
@@ -1,7 +0,0 @@
-import api from "../utils/api";
-
-export const generateWorkout = (data, token) => {
-  return api.post("/workouts", data, {
-    
-  });
-};

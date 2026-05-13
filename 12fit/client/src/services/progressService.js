1e652a4e36c2fad1e80ed1c2c889df898347edf5
Mohammad14567 <evilabosaleh@gmail.com>
2026-05-13 18:38:49 +0300

    حذف

--- 12fit/client/src/services/progressService.js
+++
@@ -1,9 +0,0 @@
-import api from "../utils/api";
-
-export const getProgress = () => {
-  return api.get("/progress");
-};
-
-export const addProgress = (data) => {
-  return api.post("/progress", data);
-};
